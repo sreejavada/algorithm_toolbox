5a7c94c3f17bbd35dcdaefbbaae8a84689896385
sreejavada <43925584+sreejavada@users.noreply.github.com>
2019-01-12 14:12:09 +0530

    week 4: 1,3,4
    Signed-off-by: sreejavada <43925584+sreejavada@users.noreply.github.com>

--- algorithm_toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
+++ algorithm_toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
@@ -4,18 +4,55 @@
 
 using std::vector;
 using std::swap;
+using namespace std;
 
-int partition2(vector<int> &a, int l, int r) {
+// comments verbose format
+//#define VERBOSE 1
+
+#ifdef VERBOSE 
+#define PRINT_VERBOSE(...) cout __VA_ARGS__
+#else
+#define PRINT_VERBOSE(...)
+#endif
+struct pair_m
+{
+	int m1;
+	int m2;
+};
+
+void three_way_swap(vector<int> &a, int index1, int index2, int index3) {
+	PRINT_VERBOSE(<< "3 WAY SWAP" << a[index1] << " " << a[index2] << " " << a[index3] << " " << endl);
+	int dummy = a[index1];
+	a[index1] = a[index3];
+	a[index3] = a[index2];
+	a[index2] = dummy;
+}
+
+pair_m partition2(vector<int> &a, int l, int r) {
   int x = a[l];
-  int j = l;
+  int m1 = l;
+  int m2 = l;
+  pair_m m;
   for (int i = l + 1; i <= r; i++) {
-    if (a[i] <= x) {
-      j++;
-      swap(a[i], a[j]);
+	  PRINT_VERBOSE(<< "i:" << i << endl);
+	  for (size_t j = l; j < r+1; ++j) {
+		  PRINT_VERBOSE(<< a[j] << ' ');
+	  }
+	  PRINT_VERBOSE(<< endl);
+	  if (a[i] == x) {
+		  swap(a[i], a[m2 + 1]);
+		  m2++;
+	}
+    if (a[i] < x) {
+		three_way_swap(a, i, m1 + 1, m2 + 1);
+		m1++;
+		m2++;
     }
   }
-  swap(a[l], a[j]);
-  return j;
+  swap(a[l], a[m1]);
+  m.m1 = m1;
+  m.m2 = m2;
+  return m;
 }
 
 void randomized_quick_sort(vector<int> &a, int l, int r) {
@@ -25,10 +62,10 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
 
   int k = l + rand() % (r - l + 1);
   swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+  pair_m m = partition2(a, l, r);
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  randomized_quick_sort(a, l, m.m1 - 1);
+  randomized_quick_sort(a, m.m2 + 1, r);
 }
 
 int main() {
--- algorithm_toolbox/week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
+++ algorithm_toolbox/week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
@@ -1,15 +1,79 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+// comments verbose format
+//#define VERBOSE 1
+
+#ifdef VERBOSE 
+#define PRINT_VERBOSE(...) cout __VA_ARGS__
+#else
+#define PRINT_VERBOSE(...)
+#endif
+
 using std::vector;
 
+long long merge_sort_inversions(vector<int> &array_to_be_sorted, vector<int> &b, size_t start, size_t mid, size_t end) {
+	PRINT_VERBOSE(<< __func__ << std::endl);
+	for (size_t i = start; i < end; i++) {
+		PRINT_VERBOSE(<< array_to_be_sorted[i] << " ");
+	}
+	PRINT_VERBOSE(<< "array ends" << endl);
+	std::vector<int> dummy_per_unit_value(end - start);
+
+	size_t i = start, j = mid;
+	long long inversions = 0;
+	int k = 0;
+	while (i < mid && j < end) {
+		
+		if (array_to_be_sorted[i] > array_to_be_sorted[j]) {
+			inversions = inversions + (end - j);
+			PRINT_VERBOSE(<< "inversions: " << inversions << endl);
+			dummy_per_unit_value[k] = array_to_be_sorted[i];
+			i++;
+			k++;
+		}
+		else {
+			dummy_per_unit_value[k] = array_to_be_sorted[j];
+			j++;
+			k++;
+		}
+	}
+
+	while (i < mid) {
+		dummy_per_unit_value[k] = array_to_be_sorted[i];
+		i++;
+		k++;
+	}
+	PRINT_VERBOSE(<< "i loop done" << endl);
+	while (j < end) {
+		dummy_per_unit_value[k] = array_to_be_sorted[j];
+		j++;
+		k++;
+	}
+	for (i = 0; i < (end - start); i++) {
+		array_to_be_sorted[start + i] = dummy_per_unit_value[i];
+	}
+	return inversions;
+}
+
+
 long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
+	for (size_t i = left; i < right; i++) {
+		PRINT_VERBOSE(<< a[i] << " ");
+	}
+	PRINT_VERBOSE(<< "array ends main func" << endl);
   long long number_of_inversions = 0;
   if (right <= left + 1) return number_of_inversions;
   size_t ave = left + (right - left) / 2;
   number_of_inversions += get_number_of_inversions(a, b, left, ave);
+  PRINT_VERBOSE(<< "number of inversions left: " << number_of_inversions << endl);
   number_of_inversions += get_number_of_inversions(a, b, ave, right);
+  PRINT_VERBOSE(<< "number of inversions right: " << number_of_inversions << endl);
   //write your code here
+  number_of_inversions += merge_sort_inversions(a, b, left, ave, right);
+
+  PRINT_VERBOSE(<< "number of inversions total: " << number_of_inversions << endl);
   return number_of_inversions;
 }
 
@@ -22,4 +86,8 @@ int main() {
   }
   vector<int> b(a.size());
   std::cout << get_number_of_inversions(a, b, 0, a.size()) << '\n';
+  for (size_t i = 0; i < a.size(); i++) {
+	  PRINT_VERBOSE(<< a[i] << " ");
+  }
+  PRINT_VERBOSE(<< endl);
 }
